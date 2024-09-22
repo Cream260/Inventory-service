f985a66eb8b73adbcad0b2189837de4c09a4fc05
Cream260 <64160260@go.buu.ac.th>
2024-09-22 23:13:07 +0700

    Change Order Status

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 import {
+  ClientProxy,
   Ctx,
   MessagePattern,
   Payload,
@@ -9,7 +10,10 @@ import {
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('ORDER_SERVICE') private orderService: ClientProxy,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -24,13 +28,17 @@ export class AppController {
     const channel = context.getChannelRef();
     const originalMsg = context.getMessage();
     console.log('Order received for processing: ', data);
-    const isInStock = false;
+    const isInStock = true;
     if (isInStock) {
       console.log('Inventory available. Processing order. ');
       channel.ack(originalMsg);
+      //Complete Order
+      this.orderService.emit('order_completed', data);
     } else {
       console.log('Inventory not available. ');
       channel.ack(originalMsg);
+      //Cancled Order
+      this.orderService.emit('order_canceled', data);
     }
   }
 }
